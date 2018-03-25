65962fd6c5f7c0ad6a8278c27e42e2e8aaaec48a
crichmond1989 <crichmond1989@gmail.com>
2018-03-25 15:40:27 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hellonetcore {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Starting!");
+
+            var delay = args?.FirstOrDefault();
+
+            int.TryParse(delay, out int delaySeconds);
+
+            if (delaySeconds < 0)
+                delaySeconds = 0;
+
+            while (delaySeconds > 0) {
+                var noun = delaySeconds == 1 ? "second" : "seconds";
+
+                Console.WriteLine($"{delaySeconds} {noun} left...");
+
+                Task.Delay(1000).Wait();
+
+                delaySeconds--;
+            }
+
+            Console.WriteLine("Complete!");
+            Console.WriteLine("Press any key to exit...");
+
+            Console.Read();
+        }
+    }
+}
