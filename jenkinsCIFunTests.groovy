b51f7db0d122fcdf423b45e957e570d8e5e2f514
crichmond1989 <crichmond1989@gmail.com>
2018-05-01 00:36:14 -0500

    switched to new testit syntax

--- jenkinsCIFunTests.groovy
+++ jenkinsCIFunTests.groovy
@@ -1,23 +1,33 @@
 import groovy.lang.Script
 
-@Library("testit")
-import testit.FunTest
+import testit.SuiteSetup
+import testit.Test
 
 class JenkinsCIFunTests implements Serializable {
     Script script
 
-    @FunTest
-    void buildArtifactsExist() {
+    @SuiteSetup
+    void suiteSetup() {
+        script.deleteDir()
+        script.unstash("source")
+
         final ciScript = script.load("jenkinsCI.groovy")
 
-        script.deleteDir()
-
         ciScript(branch: "master", script: script)
+    }
 
+    @Test
+    void buildArtifactsExist() {
         final appPath = "app/bin/Debug/netcoreapp2.0/hellonetcore.app.dll"
-        final appExists = script.fileExists(appPath)
 
-        assert appExists
+        assert script.fileExists(appPath)
+    }
+
+    @Test
+    void testResultsExist() {
+        final resultsPath = "tests/TestResults/TestResults.xml"
+        
+        assert script.fileExists(resultsPath)
     }
 }
 
