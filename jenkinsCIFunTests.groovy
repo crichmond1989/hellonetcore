3a2b5bd883a780a9213f51dfb31bfcf5553ad571
thestud2012 <crichmond1989@gmail.com>
2018-04-30 20:52:25 -0500

    added testit test

---
+++ jenkinsCIFunTests.groovy
@@ -0,0 +1,24 @@
+import groovy.lang.Script
+
+@Library("testit")
+import testit.FunTest
+
+void call(Map args) {
+    Script script
+
+    @FunTest
+    void buildArtifactsExist() {
+        final ciScript = script.load("jenkinsCI.groovy")
+
+        script.deleteDir()
+
+        ciScript(branch: "master", script: script)
+
+        final appPath = "app/bin/Debug/netcoreapp2.0/hellonetcore.app.dll"
+        final appExists = script.fileExists(appPath)
+
+        assert appExists
+    }
+}
+
+return this
